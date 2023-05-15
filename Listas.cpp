3ff609a0dd8a9dc700090152a1781bc890426319
Kenneth Cortez <kennethcortez19@hotmail.com>
2023-05-15 10:39:30 -0500

    v1.0

--- Listas.cpp
+++ Listas.cpp
@@ -1,76 +1,138 @@
-
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 struct Nodo
 {
-    int dato;
     Nodo *siguiente;
+    int dato;
 };
 
-// declaramos funciones
-void insertarLista(Nodo *&, int);
-void mostrarLista(Nodo *);
+Nodo *lista = NULL;
+int dato;
+void menu();
+void insertarlista(Nodo *&, int);
+void mostrarlista(Nodo *);
+void eliminarlista(Nodo *&, int &);
 
-// main
 int main()
 {
-    Nodo *lista = NULL;
 
-    int dato;
-    char rpt;
-
-    cout << "Ingrese un numero (0 para mostrar lista): ";
-    cin >> dato;
-
-    while (dato != 0)
-    {
-        insertarLista(lista, dato);
-        cout << "\nIngrese un numero (0 para mostrar lista): ";
-        cin >> dato;
-    }
-
-    mostrarLista(lista);
+    menu();
 
     getch();
     return 0;
 }
-// fin del main
 
-// Se crea un nuevo nodo
-void insertarLista(Nodo *&lista, int n)
+void menu()
 {
-    Nodo *nuevo_nodo = new Nodo();
-    nuevo_nodo->dato = n;
-
-    Nodo *i = lista;
-
-    if (lista == NULL)
-        lista = nuevo_nodo;
-    else
+    int opc;
+    do
     {
-        while (i->siguiente != NULL)
-            i = i->siguiente;
-        i->siguiente = nuevo_nodo;
-    }
+        cout << "\t.:MENU:.\n";
+        cout << "1. Insertar elemento en la lista." << endl;
+        cout << "2. Mostrar elementos de la lista." << endl;
+        cout << "3. Vaciar la lista" << endl;
+        cout << "4. Salir" << endl;
+        cin >> opc;
 
-    cout << "Elemento " << n << " insertado a lista correctamente\n";
+        switch (opc)
+        {
+        case 1:
+            cout << "Digite el elemento a ingresar: ";
+            cin >> dato;
+            insertarlista(lista, dato);
+            cout << "\n";
+            system("pause");
+            break;
+
+        case 2:
+            cout << "\tMostrando elementos de la lista\n"
+                 << endl;
+            mostrarlista(lista);
+            cout << "\n";
+            system("pause");
+            break;
+
+            system("pause");
+            break;
+        case 3:
+            eliminarlista(lista, dato);
+            cout << "\n";
+            system("pause");
+            break;
+        case 4:
+            break;
+        }
+        system("cls");
+    } while (opc != 4);
 }
 
-// Se crea un nuevo nodo(actual)
-void mostrarLista(Nodo *lista)
+void insertarlista(Nodo *&lista, int n)
 {
-    Nodo *actual = new Nodo();
+    Nodo *nuevo_nodo = new Nodo();
 
-    // se iguala ese nuevo nodo(actual) a lsita
-    actual = lista;
+    nuevo_nodo->dato = n;
+    nuevo_nodo->siguiente = NULL;
 
-    // Se recorre la lista de inicio a fin
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+
+    while ((aux1 != NULL) && (aux1->dato < n))
+    {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    if (lista == aux1)
+    {
+        lista = nuevo_nodo;
+    }
+    else
+    {
+        aux2->siguiente = nuevo_nodo;
+    }
+    nuevo_nodo->siguiente = aux1;
+
+    cout << n << " se ha agregado correctamente a la lista." << endl;
+}
+
+void mostrarlista(Nodo *lista)
+{
+    Nodo *actual = lista;
+
     while (actual != NULL)
     {
         cout << actual->dato << " -> ";
         actual = actual->siguiente;
+    }
+}
+
+void eliminarlista(Nodo *&lista, int &n)
+{
+    char opc;
+
+    cout << "¿Seguro de eliminar todos los elementos de la lista? (S/N): ";
+    cin >> opc;
+
+    if (((opc == 'S') || (opc == 's')))
+
+        while (lista != NULL)
+        {
+            Nodo *aux = lista;
+            n = aux->dato;
+            lista = aux->siguiente;
+            delete aux;
+        }
+    if (((opc == 'S') || (opc == 's')))
+    {
+        cout << "\tTodos los elementos de la lista han sido eliminados.\n"
+             << endl;
+    }
+    else
+    {
+        cout << "\tVolver.\n"
+             << endl;
     }
 }
